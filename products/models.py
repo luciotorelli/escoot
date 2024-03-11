253ce2c489c519217dd8ad51854e80e95042bc8d
Lucio Torelli <luciotorelli@hotmail.com>
2024-03-11 13:09:48 +0000

    Create products models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.IntegerField()
+    image = models.ImageField(upload_to='product_images/')
+    status = models.CharField(max_length=255)
+    product_category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.product_name
