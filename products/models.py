86823dc6790cc509ef73694d96aa9f24085de416
Lucio Torelli <luciotorelli@hotmail.com>
2024-03-14 17:07:22 +0000

    Fix integer typo error

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock_quantity = models.IntegerField(max_length=9999)  # Corrected typo here
+    stock_quantity = models.IntegerField()
     image = CloudinaryField('image')  # Uses CloudinaryField
     STATUS_CHOICES = [
         ('in-stock', 'Stocked'),
