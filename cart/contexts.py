c5de3eec29413864fcbe498f929d5efaaa9e05cf
Lucio Torelli <luciotorelli@hotmail.com>
2024-04-24 15:31:16 +0100

    Update cart contexts and add product count to cart nav icon

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,18 +1,32 @@
-# cart/context.py
-
+from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
     cart_items = []
-    total = 0 
-    product_count = 0 
+    total = 0
+    product_count = 0
+    cart = request.session.get('cart', {})
 
-    # Delivery cost
-    delivery_threshold = settings.FREE_DELIVERY_THRESHOLD
-    standard_delivery_percentage = settings.STANDARD_DELIVERY_PERCENTAGE
-    free_delivery_delta = 0
-    delivery = 0
-    grand_total = 0
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
 
     context = {
         'cart_items': cart_items,
@@ -20,7 +34,8 @@ def cart_contents(request):
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': delivery_threshold,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
+
     return context
--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 # cart/urls.py
 from django.urls import path
-from .views import cart
+from .views import cart, add_to_cart
 
 app_name = 'cart'
 
 urlpatterns = [
     path('', cart, name='cart'),
+    path('add/<item_id>/', add_to_cart, name='add_to_cart'),    
 ]
