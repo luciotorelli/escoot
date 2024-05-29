80eb6d4cdfa2a5965b3ea2c8e5157a82ce72e566
Lucio Torelli <luciotorelli@hotmail.com>
2024-05-29 02:17:13 +0100

    Add admin.py to view contact us messages and fix contact_email_body.txt comment

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'date_submitted', 'order')
+    list_filter = ('date_submitted', 'order')
+    search_fields = ('name', 'email', 'message')
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
