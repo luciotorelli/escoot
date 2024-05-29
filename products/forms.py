227e78e24ea8cfcd443d4fea68c266bb39fb19ed
Lucio Torelli <luciotorelli@hotmail.com>
2024-05-29 21:03:19 +0100

    Remove rounded-0 styling for consistency

--- products/forms.py
+++ products/forms.py
@@ -11,4 +11,4 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'border-black'
